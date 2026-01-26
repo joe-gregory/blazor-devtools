b0f3b90f039c1fc30faa98722e7a0d55e3f0e7a5
joe-gregory <josephedward2nd@gmail.com>
2026-01-26 15:48:33 -0800

    docs site is not server render mode

---
+++ BlazorDevTools.Docs.Site.Server/Program.cs
@@ -0,0 +1,39 @@
+using BlazorDeveloperTools;
+using BlazorDevTools.Docs.Site.Server.Components;
+
+namespace BlazorDevTools.Docs.Site.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddBlazorDevTools();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
