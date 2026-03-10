a9422c01934454779d57e65ef98d2c594b2b0750
joe-gregory <josephedward2nd@gmail.com>
2026-03-09 19:38:12 -0700

    Cleaned up src/browser-extensions/dist from git

--- src/browser-extensions/dist/chromium/bridge.d.ts
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=bridge.d.ts.map
--- src/browser-extensions/dist/core/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export { BlazorBridge } from './blazor-bridge';
-export { ComponentService } from './component-service';
-export { EventEmitter } from './event-emitter';
-export type { EventHandler, EventFilter, Subscription } from './event-emitter';
-//# sourceMappingURL=index.d.ts.map
